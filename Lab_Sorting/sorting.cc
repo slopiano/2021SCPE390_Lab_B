47a965dbb73ab3eb61460e9631dbfbca781e20d0
MINGXUAN CHEN <mingxuanchen@MINGXUANdeMacBook-Pro-2.local>
2021-04-07 23:39:23 -0400

    add Lab_Sorting

---
+++ Lab_Sorting/sorting.cc
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+void bsort(int a[],int n)
+{
+
+}
+void output(int a[],int n)
+{
+
+}
+int main()
+{
+    int a[6] = {2,4,1,9,6,3};
+    int n=6;
+    bsort(a,n);
+    output(a,n);
+    
+    return 0;
+}
